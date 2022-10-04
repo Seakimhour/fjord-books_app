cb7084d208e844ac50665a2b73b1ca0791966315
Sea Kimhour <seakimhour@gmail.com>
2022-10-04 18:21:31 +0900

    fix devise params permit

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  before_action :configure_params_permit
 
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
+  def configure_params_permit
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[postal_code address self_introduction])
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
   end
 end
