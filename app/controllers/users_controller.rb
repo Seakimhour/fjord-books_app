8cd9f66f27b144cc96385df325e4cdc341a0d9ec
Sea Kimhour <seakimhour@gmail.com>
2022-10-12 11:16:53 +0900

    コード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.json
   def index
     @users = User.order(:id).page(params[:page])
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
-    if User.exists?(id: params[:id])
-      @user = User.find(params[:id])
-    else
-      redirect_to users_path
-    end
+    @user = User.find(params[:id])
   end
 
-  # GET /
   def profile
     if current_user
       @user = current_user
