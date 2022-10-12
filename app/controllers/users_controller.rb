336eec6b3f7d8c6ed854440b9b17ed0432161d82
Sea Kimhour <seakimhour@gmail.com>
2022-10-12 15:50:55 +0900

    コード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
-
-    @users.each do |user|
-      current_user.followings.each do |follow|
-        user.followed = true if user == follow
-      end
-    end
   end
 
   def show
     @user = User.find(params[:id])
-
-    current_user.followings.each do |follow|
-      @user.followed = true if @user == follow
-    end
   end
 
   def followers
@@ -28,12 +18,12 @@ class UsersController < ApplicationController
   end
 
   def follow
-    Follow.create(follower_id: current_user.id, following_id: params[:id])
+    current_user.following_follows.create!(following_id: params[:id])
     redirect_back(fallback_location: users_path, notice: t('controllers.common.notice_follow'))
   end
 
   def unfollow
-    Follow.find_by(follower_id: current_user.id, following_id: params[:id]).destroy
+    current_user.following_follows.find_by!(following_id: params[:id]).destroy
     redirect_back(fallback_location: users_path, notice: t('controllers.common.notice_unfollow'))
   end
 end
